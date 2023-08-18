e5a9bed16b5ff025a805d1b549060cc10d9c0ce6
bianjh <bianjh@nih.gov>
2023-08-18 09:43:59 -0400

    update modScore code, add more unit test

--- tests/testthat/test-ModuleScore.R
+++ tests/testthat/test-ModuleScore.R
@@ -1,41 +1,38 @@
 # Dataset Testing
 test_that("modScore returns metadata with scores and cell calls for tec", {
-  
+
   tec <- getModuleScoreParam("tec")
-  
+
   suppressWarnings(modscore.demo <- do.call(modScore, tec))
-  
+
   skip_on_ci()
   expect_snapshot_file(
     .drawMSfig(modscore.demo$ms.figures[[1]]),
     "tec_MS_1.png"
   )
-  
+
   skip_on_ci()
   expect_snapshot_file(
     .drawMSfig(modscore.demo$ms.figures[[2]]),
     "tec_MS_2.png"
   )
-  
+
   skip_on_ci()
   expect_snapshot_file(
     .drawMSfig(modscore.demo$ms.figures[[3]]),
     "tec_MS_3.png"
   )
-  
-  expect_equal(mean(modscore.demo$ms.object$CD8_T),
-               0.044506, tolerance = 1e-1)
-  
-  expect_equal(mean(modscore.demo$ms.object$CD4_T),
-               0.044506, tolerance = 1e-1)
-  
-  expect_equal(mean(modscore.demo$ms.object$Tregs),
-               0.0913447, tolerance = 1e-1)
-  
+
+  saveRDS(modscore.demo$harm.object,
+          file = test_path("output/modScore/TEC_so.rds"))
+
+  expect_equal_to_reference(modscore.demo$harm.object,
+                            file = test_path("output/modScore/TEC_so.rds"))
+
   expected_elements <- c("Likely_CellType",tec$celltypes)
-  expect(all(expected_elements %in% colnames(modscore.demo$ms.object@meta.data)), 
+  expect(all(expected_elements %in% colnames(modscore.demo$ms.object@meta.data)),
                           failure_message = "modscore results not found")
-  
+
 })
 
 test_that("modScore returns metadata with scores and cell calls for chariou", {
@@ -49,27 +46,25 @@ test_that("modScore returns metadata with scores and cell calls for chariou", {
     .drawMSfig(modscore.demo$ms.figures[[1]]),
     "chariou_MS_1.png"
   )
-  
+
   skip_on_ci()
   expect_snapshot_file(
     .drawMSfig(modscore.demo$ms.figures[[2]]),
     "chariou_MS_2.png"
   )
-  
+
   skip_on_ci()
   expect_snapshot_file(
     .drawMSfig(modscore.demo$ms.figures[[3]]),
     "chariou_MS_3.png"
   )
 
-  expect_equal(mean(modscore.demo$ms.object$CD8_T),
-               0.05458893, tolerance = 1e-1)
 
-  expect_equal(mean(modscore.demo$ms.object$CD4_T),
-               0.05458893, tolerance = 1e-1)
+  saveRDS(modscore.demo$harm.object,
+          file = test_path("output/modScore/chariou_so.rds"))
 
-  expect_equal(mean(modscore.demo$ms.object$Tregs),
-               0.07577882, tolerance = 1e-1)
+  expect_equal_to_reference(modscore.demo$harm.object,
+                            file = test_path("output/modScore/chariou_so.rds"))
 
   expected_elements <- c("Likely_CellType",chariou$celltypes)
   expect(all(expected_elements %in% colnames(modscore.demo$ms.object@meta.data)),
@@ -89,27 +84,24 @@ test_that("modScore returns metadata with scores and cell calls for
     .drawMSfig(modscore.demo$ms.figures[[1]]),
     "pbmc_single_MS_1.png"
   )
-  
+
   skip_on_ci()
   expect_snapshot_file(
     .drawMSfig(modscore.demo$ms.figures[[2]]),
     "pbmc_single_MS_2.png"
   )
-  
+
   skip_on_ci()
   expect_snapshot_file(
     .drawMSfig(modscore.demo$ms.figures[[3]]),
     "pbmc_single_MS_3.png"
   )
 
-  expect_equal(mean(modscore.demo$ms.object$rand_type1),
-               0.2129345, tolerance = 1e-1)
+  saveRDS(modscore.demo$harm.object,
+          file = test_path("output/modScore/pbmc_single_so.rds"))
 
-  expect_equal(mean(modscore.demo$ms.object$rand_type2),
-               0.1917001, tolerance = 1e-1)
-
-  expect_equal(mean(modscore.demo$ms.object$rand_type3),
-               0.1798533, tolerance = 1e-1)
+  expect_equal_to_reference(modscore.demo$harm.object, file =
+                              test_path("output/modScore/pbmc_single_so.rds"))
 
   expected_elements <- c("Likely_CellType",pbmc.single$celltypes)
   expect(all(expected_elements %in% colnames(modscore.demo$ms.object@meta.data)),
@@ -129,27 +121,24 @@ test_that("modScore returns metadata with scores and cell calls for
     .drawMSfig(modscore.demo$ms.figures[[1]]),
     "nsclc_multi_MS_1.png"
   )
-  
+
   skip_on_ci()
   expect_snapshot_file(
     .drawMSfig(modscore.demo$ms.figures[[2]]),
     "nsclc_multi_MS_2.png"
   )
-  
+
   skip_on_ci()
   expect_snapshot_file(
     .drawMSfig(modscore.demo$ms.figures[[3]]),
     "nsclc_multi_MS_3.png"
   )
 
-  expect_equal(mean(modscore.demo$ms.object$rand_type1),
-               0.1266389, tolerance = 1e-1)
+  saveRDS(modscore.demo$harm.object,
+          file = test_path("output/modScore/nsclc_multi_so.rds"))
 
-  expect_equal(mean(modscore.demo$ms.object$rand_type2),
-               0.1794167, tolerance = 1e-1)
-
-  expect_equal(mean(modscore.demo$ms.object$rand_type3),
-               0.2627949, tolerance = 1e-1)
+  expect_equal_to_reference(modscore.demo$harm.object, file =
+                              test_path("output/modScore/nsclc_multi_so.rds"))
 
   expected_elements <- c("Likely_CellType",nsclc.multi$celltypes)
   expect(all(expected_elements %in% colnames(modscore.demo$ms.object@meta.data)),
@@ -166,29 +155,26 @@ test_that("modScore returns metadata with scores and cell calls for brca", {
   skip_on_ci()
   expect_snapshot_file(
     .drawMSfig(modscore.demo$ms.figures[[1]]),
-    "brca_multi_MS_1.png"
+    "brca_MS_1.png"
   )
-  
+
   skip_on_ci()
   expect_snapshot_file(
     .drawMSfig(modscore.demo$ms.figures[[2]]),
-    "brca_multi_MS_2.png"
+    "brca_MS_2.png"
   )
-  
+
   skip_on_ci()
   expect_snapshot_file(
     .drawMSfig(modscore.demo$ms.figures[[3]]),
-    "brca_multi_MS_3.png"
+    "brca_MS_3.png"
   )
 
-  expect_equal(mean(modscore.demo$ms.object$rand_type1),
-               0.1958383, tolerance = 1e-1)
+  saveRDS(modscore.demo$harm.object,
+          file = test_path("output/modScore/brca_so.rds"))
 
-  expect_equal(mean(modscore.demo$ms.object$rand_type2),
-               0.1079014, tolerance = 1e-1)
-
-  expect_equal(mean(modscore.demo$ms.object$rand_type3),
-               0.03652896, tolerance = 1e-1)
+  expect_equal_to_reference(modscore.demo$harm.object, file =
+                              test_path("output/modScore/brca_so.rds"))
 
   expected_elements <- c("Likely_CellType",brca$celltypes)
   expect(all(expected_elements %in% colnames(modscore.demo$ms.object@meta.data)),
@@ -208,9 +194,43 @@ test_that("modScore detects when no genes are found in the data", {
     marker.table = apply(chariou$marker.table,2, function(x) toupper(x)),
     celltypes = chariou$celltypes,
     general.class = chariou$general.class,
-    lvl.df = chariou$levels_df_demo,
+    lvl.df = chariou$lvl.df,
     nbins = 10), "No genes from list was found in data"))
 
+})
+
+test_that("modScore runs hierarchical classification", {
+  
+  tec <- getModuleScoreParam("tec")
+  
+  modscore.demo <- modScore(
+    object = tec$object,
+    samples.subset = tec$samples.subset,
+    sample.to.display = tec$sample.to.display,
+    marker.table = tec$marker.table,
+    celltypes = c("CD8_T","CD4_T","Tregs"),
+    multi.lvl = TRUE,
+    general.class = c("CD8_T","CD4_T"),
+    lvl.df = tec$lvl.df,
+    threshold = c(0.1,0.1,0.1),
+    nbins = 10)
+  
+  skip_on_ci()
+  expect_snapshot_file(
+    .drawMSfig(modscore.demo$ms.figures[[4]]),
+    "tec_MS_hierarchical_1.png"
+  )
+  
+  saveRDS(modscore.demo$harm.object,
+          file = test_path("output/modScore/TEC_so.rds"))
+  
+  expect_equal_to_reference(modscore.demo$harm.object,
+                            file = test_path("output/modScore/TEC_so.rds"))
+  
+  expected_elements <- c("Likely_CellType",tec$celltypes)
+  expect(all(expected_elements %in% colnames(modscore.demo$ms.object@meta.data)), 
+         failure_message = "modscore results not found")
+  
 })
 
 test_that("modScore detects when threshold number does not match number of
@@ -225,7 +245,7 @@ test_that("modScore detects when threshold number does not match number of
     marker.table = chariou$marker.table,
     celltypes = chariou$celltypes,
     general.class = chariou$general.class,
-    lvl.df = chariou$levels_df_demo,
+    lvl.df = chariou$lvl.df,
     threshold = rep(0.1,5),
     nbins = 10),
     "Threshold length does not match # celltypes to analyze"))
